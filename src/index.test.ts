000179a85ce631b62a574f1dc61add073c717b50
zhouyingchao <1212>
2021-02-03 17:29:54 +0800

    ✨feat(自动化测试):编写测试用例

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,8 @@
 import { join } from 'path';
 import { Service } from 'umi';
 import { render, fireEvent, screen } from '@testing-library/react';
-import { renderHook, act } from '@testing-library/react-hooks';
 import React from 'react';
+import { readFileSync } from 'fs';
 
 const fixtures = join(__dirname, './fixtures');
 
@@ -35,4 +35,20 @@ test('生成临时文件', async () => {
   ));
 
   expect(data).toHaveProperty('InputAutoCompleteCustom');
+});
+
+test('测试生成html', async () => {
+  const cwd = join(fixtures, 'normal');
+  const service = new Service({
+    cwd,
+    plugins: [require.resolve('./')],
+  });
+  await service.run({
+    name: 'g',
+    args: {
+      _: ['g', 'html'],
+    },
+  });
+  const html = readFileSync(join(cwd, 'dist', 'index.html'), 'utf-8');
+  expect(html).toMatch(/umi\.js/);
 });
